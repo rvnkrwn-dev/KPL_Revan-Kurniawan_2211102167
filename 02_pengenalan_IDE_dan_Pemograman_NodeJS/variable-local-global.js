105479c732f0f0d50120051d99a2b9ddae268908
Revan Kurniawan <rvnkrwn@hotmail.com>
2025-02-24 12:06:32 +0700

    praktikum 02

---
+++ 02_pengenalan_IDE_dan_Pemograman_NodeJS/variable-local-global.js
@@ -0,0 +1,12 @@
+let globalVarible = "Revan Kurniawan"
+
+function getMyIdentity() {
+    let localVariable = 2211102167
+
+    console.log(`Nama saya ${globalVarible} nim ${localVariable}`)
+}
+
+console.log(globalVarible)
+// console.log(localVariable)
+
+getMyIdentity()
---
+++ 02_pengenalan_IDE_dan_Pemograman_NodeJS/variables.js
@@ -0,0 +1,27 @@
+// var
+
+var name = "Revan Kurniawan"
+const nim = 2211102167
+
+console.log(name)
+
+console.log("nama saya adalah " + name + " dan nim saya adalah " + nim)
+console.log(`Nama saya adalah ${name} dan nim saya adalah ${nim}`)
+
+console.log(`Nama saya adalah ${name}
+    nim saya adalah ${nim}`)
+
+
+var name = "ayam"
+
+let fullName = "Revan Kurniawan"
+
+
+// const kelas
+// nim = 2138948;
+
+/*
+dsufhkudsf
+
+sjdhfjusdf
+*/
